0eadfd0674ae5bbdd41c75c5a72a2494d860bf7f
John Melody Me <Johnmelodyme@yandex.com>
2021-05-28 01:01:48 +0800

    Added Crawly.Spider Framework

--- lib/shopee.ex
+++ lib/shopee.ex
@@ -1,5 +1,24 @@
 defmodule Shopee do
-  def show do
+  @behaviour Crawly.Spider
+
+  @impl Crawly.Spider
+  def base_url() do
+    "https://shopee.com.my"
+  end
+
+  @impl Crawly.Spider
+  def init() do
+    [
+      start_urls: ["https://shopee.com.my/search?keyword=computer"]
+    ]
+  end
+
+  @impl Crawly.Spider
+  def parse_item(_response) do
+    %Crawly.ParsedItem{:items => [], :requests => []}
+  end
+
+  def show() do
     case HTTPoison.get("https://shopee.com.my/search?keyword=computer") do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         urls =
